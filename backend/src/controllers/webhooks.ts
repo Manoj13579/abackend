7f63f1e07018e94f6800812db1a80164af162dc4
Manoj13579 <duwaikoho.4hai@gmail.com>
2025-03-28 20:22:32 +0545

    debug

--- backend/src/controllers/webhooks.ts
+++ backend/src/controllers/webhooks.ts
@@ -43,10 +43,7 @@ If the payment succeeds, Stripe sends an event with type payment_intent.succeede
                 payment_intent: paymentIntentId,
             })
 
-            const metadata = session.data[0]?.metadata as Record<string, string> | null;
-            const purchaseId = metadata?.purchaseId;
-            
-           console.log("purchaseId", purchaseId);
+            const purchaseId = session.data[0].metadata?.purchaseId;
            
           
            const purchaseData = await Purchase.findById(purchaseId);
@@ -74,8 +71,7 @@ If the payment succeeds, Stripe sends an event with type payment_intent.succeede
             payment_intent: paymentIntentId,
         })
 
-        const metadata = session.data[0]?.metadata as Record<string, string> | null;
-        const purchaseId = metadata?.purchaseId;
+        const purchaseId = session.data[0].metadata?.purchaseId;
         
        const purchaseData = await Purchase.findById(purchaseId);
        // if used for ts
